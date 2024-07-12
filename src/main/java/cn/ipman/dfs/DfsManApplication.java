23a5aa38a3ed041971398ecde248e0bc654612a8
ipipman <ipipman@163.com>
2024-07-13 05:56:12 +0800

    init dfs project

---
+++ src/main/java/cn/ipman/dfs/DfsManApplication.java
@@ -0,0 +1,13 @@
+package cn.ipman.dfs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DfsManApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DfsManApplication.class, args);
+    }
+
+}
---
+++ src/test/java/cn/ipman/dfs/DfsManApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.ipman.dfs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DfsManApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
