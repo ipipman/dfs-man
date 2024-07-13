c6ea489dba92f124d83356f7bbb854948fcba01d
ipipman <ipipman@163.com>
2024-07-13 09:29:26 +0800

    add download api

--- src/main/java/cn/ipman/dfs/FileController.java
+++ src/main/java/cn/ipman/dfs/FileController.java
@@ -1,13 +1,15 @@
 package cn.ipman.dfs;
 
+import jakarta.servlet.http.HttpServletResponse;
 import lombok.SneakyThrows;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
-import java.io.File;
+import java.io.*;
 
 /**
  * Description for this class
@@ -34,6 +36,34 @@ public class FileController {
         System.out.println(uploadPath + "/" + filename);
         file.transferTo(uploadFile);
         return filename;
+    }
+
+    @RequestMapping("/download")
+    public void download(String name, HttpServletResponse response) {
+        String path = uploadPath + "/" + name;
+        File file = new File(path);
+        try {
+            FileInputStream inputStream = new FileInputStream(file);
+            InputStream fis = new BufferedInputStream(inputStream);
+            byte[] buffer = new byte[16 * 1024];
+
+            // 加一些response的头
+            response.setCharacterEncoding("UTF-8");
+            response.setContentType("application/octet-stream");
+            response.setHeader("Content-Disposition", "attachment;filename=" + name);
+            response.setHeader("Content-Length", String.valueOf(file.length()));
+
+            // 读取文件信息, 并逐段输出
+            OutputStream outputStream = response.getOutputStream();
+            while (fis.read(buffer) != -1) {
+                outputStream.write(buffer);
+            }
+            outputStream.flush();
+
+            fis.close();
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
     }
 
 
