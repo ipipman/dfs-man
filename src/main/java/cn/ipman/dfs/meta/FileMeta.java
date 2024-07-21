e241198df7c875d8f85482c1e6e84c0a5491eae9
ipipman <ipipman@163.com>
2024-07-21 13:03:34 +0800

    polish meta

--- src/main/java/cn/ipman/dfs/meta/FileMeta.java
+++ src/main/java/cn/ipman/dfs/meta/FileMeta.java
@@ -8,7 +8,8 @@ import java.util.HashMap;
 import java.util.Map;
 
 /**
- * file meta data.
+ * 文件元数据类，用于存储文件的元信息。
+ * 包括文件名、原始文件名、文件大小、下载链接以及标签。
  *
  * @Author IpMan
  * @Date 2024/7/20 14:59
@@ -18,10 +19,30 @@ import java.util.Map;
 @NoArgsConstructor
 public class FileMeta {
 
+    /**
+     * 文件名，用于唯一标识一个文件。
+     */
     private String name;
+
+    /**
+     * 原始文件名，保留用户上传时的文件名。
+     */
     private String originalFileName;
+
+    /**
+     * 文件大小，以字节为单位。
+     */
     private long size;
+
+    /**
+     * 文件下载URL，提供文件下载的地址。
+     */
     private String downloadUrl;
+
+    /**
+     * 文件标签，使用Map存储，键值对形式表示，用于标记或分类文件。
+     * 标签可用于快速搜索或筛选文件。
+     */
     private Map<String, String> tags = new HashMap<>();
 
 }
