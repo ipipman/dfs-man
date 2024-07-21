7fccd4e27b7a36a656465f2c66ca6e789272927e
ipipman <ipipman@163.com>
2024-07-21 13:01:39 +0800

    polish config

--- src/main/java/cn/ipman/dfs/config/DfsConfig.java
+++ src/main/java/cn/ipman/dfs/config/DfsConfig.java
@@ -5,13 +5,20 @@ import org.springframework.boot.web.servlet.MultipartConfigFactory;
 import org.springframework.context.annotation.Bean;
 
 /**
- * Description for this class
+ * 该类用于在应用中配置文件上传设置。
  *
  * @Author IpMan
  * @Date 2024/7/13 10:03
  */
 public class DfsConfig {
 
+    /**
+     * 创建并配置MultipartConfigElement Bean。
+     * 此配置用于指定临时存储上传文件的位置，
+     * 以及与文件上传相关的其他设置。
+     *
+     * @return MultipartConfigElement 配置好的多部分配置元素
+     */
     @Bean
     MultipartConfigElement multipartConfigElement() {
         MultipartConfigFactory factory = new MultipartConfigFactory();
--- src/main/java/cn/ipman/dfs/config/DfsConfigProperties.java
+++ src/main/java/cn/ipman/dfs/config/DfsConfigProperties.java
@@ -4,7 +4,8 @@ import lombok.Data;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 
 /**
- * Description for this class
+ * 分布式文件系统（DFS）配置属性类。
+ * 用于绑定配置文件中以"dfs"为前缀的属性值。
  *
  * @Author IpMan
  * @Date 2024/7/21 12:43
@@ -12,10 +13,34 @@ import org.springframework.boot.context.properties.ConfigurationProperties;
 @ConfigurationProperties(prefix = "dfs")
 @Data
 public class DfsConfigProperties {
+    /**
+     * 文件上传路径配置。
+     */
     private String uploadPath;
+
+    /**
+     * 文件备份URL配置。
+     */
     private String backupUrl;
+
+    /**
+     * 文件下载URL配置。
+     */
     private String downloadUrl;
+
+    /**
+     * 文件分组配置。
+     */
     private String group;
+
+    /**
+     * 是否自动计算文件MD5配置。
+     */
     private boolean autoMd5;
+
+    /**
+     * 是否同步备份配置。
+     * 开启后，文件上传时会同步进行备份，提高数据的安全性。
+     */
     private boolean syncBackup;
 }
